53872db1b35db7cdb57735f5695916eefcee0084
Peter <peter@nextsteps.co.za>
2016-03-05 17:50:56 +0200

    Initial Project committed

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace ConsoleApplication1
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Program Calculates the result of two numbers");
+
+            Console.WriteLine("Enter Number 1: ");
+            var number1String = Console.ReadLine();
+            var number1 = 0;
+            int.TryParse(number1String, out number1);
+
+            Console.WriteLine("Enter Number 2: ");
+            var number2String = Console.ReadLine();
+            var number2 = 0;
+            int.TryParse(number2String, out number2);
+
+
+            var calculator = new Calculator();
+            int result = calculator.Sum(number1, number2);
+
+            Console.WriteLine(string.Format("Result is: {0}", result.ToString()));
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using Utils;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void one_plus_one_equal_two()
+        {
+            var calc = new Calculator();
+            var result = calc.Sum(1, 1);
+            Assert.AreEqual(result, 2);
+        }
+    }
+}
