12c34506885fc84d5d1fd9ab4e3f4fe8e3f67517
Peter <peter@nextsteps.co.za>
2016-07-02 15:18:13 +0200

    Initial commit

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace NextSteps
+{
+    static class Memiomisation
+    {
+        public static long FindPrimeNumber(int n)
+        {
+            int count = 0;
+            long a = 2;
+            while (count < n)
+            {
+                long b = 2;
+                int prime = 1;
+                while (b * b <= a)
+                {
+                    if (a % b == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                    b++;
+                }
+                if (prime > 0)
+                {
+                    count++;
+                }
+                a++;
+            }
+            return (--a);
+        }
+
+        static void Main()
+        {
+            int reps = 10;
+            int its = 50000;
+
+            Measure("baseline", reps, () =>
+            {
+                for (int i = 0; i < its; ++i)
+                {
+                    FindPrimeNumber(45);
+                }
+            });
+
+            GC.Collect();
+            GC.WaitForFullGCComplete();
+            GC.Collect();
+              
+
+            var memoFunc = Utils.Memoize<int, long>(FindPrimeNumber);
+            Measure("memo", reps, () =>
+            {
+                for (int i = 0; i < its; ++i)
+                {
+                    memoFunc(45);
+                }
+            });
+
+            Console.ReadLine();
+        }
+
+        private static void Measure(string what, int reps, Action action)
+        {
+            action(); //warm up
+
+            double[] results = new double[reps];
+            for (int i = 0; i < reps; ++i)
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                action();
+                results[i] = sw.Elapsed.TotalMilliseconds;
+            }
+            Console.WriteLine("{0} - AVG = {1}, MIN = {2}, MAX = {3}", 
+                what, results.Average(), results.Min(), results.Max());
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NextSteps
+{
+    public static class Utils
+    {
+        public static Func<Arg, Ret> Memoize<Arg, Ret>( this Func<Arg, Ret> functor)
+        {
+            var memoTable = new Dictionary<Arg, Ret>();
+
+            return (arg0) =>
+            {
+                Ret funcReturnValue;
+
+                if (!memoTable.TryGetValue(arg0, out funcReturnValue))
+                {
+                    funcReturnValue = functor(arg0);
+                    memoTable.Add(arg0, funcReturnValue);
+                }
+                return funcReturnValue;
+            };
+        }
+    }
+}
